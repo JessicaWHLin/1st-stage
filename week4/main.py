823f8c1a769de70f0e7aee7531cbfdbccf112ccd
Jessica <155356442+JessicaWHLin@users.noreply.github.com>
2024-04-26 11:42:25 +0800

    Add files via upload

--- week4/main.py
+++ week4/main.py
@@ -3,8 +3,6 @@ from fastapi.responses import HTMLResponse
 from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from starlette.applications import Starlette
-from starlette.middleware import Middleware
 from starlette.middleware.sessions import SessionMiddleware
 app = FastAPI()
 
@@ -12,8 +10,7 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 app.add_middleware(SessionMiddleware, secret_key="myscret_")
-sign_in=False
-verify=None
+
 def verify_password(username: str, password: str):
     if username == "test" and password == "1234":
         return True
@@ -27,14 +24,14 @@ async def homepage(request: Request):
 @app.post("/signin" , response_class=RedirectResponse)
 async def signin(request: Request, username : str=Form(None), password: str= Form(None)):
         verify=verify_password(username,password)
-        global sign_in
         if not username or not password:
-               return RedirectResponse(url="/error?message=請輸入帳號或密碼", status_code=303)
+                request.session["signed_in"]=False
+                return RedirectResponse(url="/error?message=請輸入帳號或密碼", status_code=303)
         if verify==False :
-                sign_in=False
+                request.session["signed_in"]=False
                 return RedirectResponse(url="/error?message=帳號，或密碼錯誤", status_code=303)
         if verify==True :
-                sign_in=True
+                request.session["signed_in"]=True
                 return RedirectResponse(url="/member", status_code=303)
       
 @app.get("/error")
@@ -42,23 +39,15 @@ async def error(request: Request,message: str):
     return templates.TemplateResponse("error.html", {"request":request, "message": message})
 @app.get("/member", response_class=RedirectResponse)
 async def success(request: Request):
-        global sign_in
-        if request.session["SIGNED-IN"]==False:
-                return RedirectResponse(url="/signout", status_code=303)
-        if request.session["SIGNED-IN"]==True:
-                return templates.TemplateResponse("member.html", {"request": request})             
+        if request.session.get("signed_in")==True:
+                 return templates.TemplateResponse(request=request, name="member.html")
+        if request.session.get("signed_in")!=True: 
+                return RedirectResponse(url="/", status_code=303)            
 
 @app.get("/signout",response_class=RedirectResponse)
-async def signout(request: Request):
-        global sign_in
-        print("第一次= ",sign_in)
-        if sign_in==True :
-                request.session["SIGNED-IN"]=True
-                sign_in=False
-                return RedirectResponse(url="/member", status_code=303)
-        if sign_in==False :
-                request.session["SIGNED-IN"]=False
-                sign_in==False
+async def signout(request: Request):    
+        if request.session.get("signed_in")==True : #點擊登出系統，要將status改為登出
+                request.session["signed_in"]=False
                 return RedirectResponse(url="/", status_code=303)
         
         
--- week4/static/script.js
+++ week4/static/script.js
@@ -2,13 +2,13 @@ document.addEventListener("DOMContentLoaded", function(event) {
   let submitButton=document.querySelector("#result");
   let checkBox=document.querySelector(".checkbox");
   if(submitButton){
-  submitButton.addEventListener('submit',function(event){
-      if (!checkBox.checked){
-        event.preventDefault();
-        alert("Please check the checkbox first.");
-      }
-    });
-  }
+    submitButton.addEventListener('submit',function(event){
+        if (!checkBox.checked){
+          event.preventDefault();
+          alert("Please check the checkbox first.");
+        }
+      });
+    }
 });
 
 
